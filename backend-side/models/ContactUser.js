3d165f19c1724e0aa33cd493572ce77a6de11196
hannamalana98 <hannamalana98@gmail.com>
2025-05-18 05:05:33 +0800

    fixed: applied MVC pattern clean up the code for readability

--- backend-side/models/ContactUser.js
+++ backend-side/models/ContactUser.js
@@ -1,14 +1,36 @@
 import mongoose from "mongoose";
 
 const contactSchema = new mongoose.Schema({
-    fullname: { type: String},
-    email: { type: String, unique: true},
-    password: { type: String },
-    role: { type: String, enum: ['Family', 'Caregiver'], required: true},
-    created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'MainUser', required: true},
-    invitationToken: { type: String},
-    isActive: { type: Boolean, default: false}
-}, { timestamps: true});
+    fullname: { 
+        type: String
+    },
+    email: { 
+        type: String, 
+        unique: true
+    },
+    password: { 
+        type: String 
+    },
+    role: 
+    { type: String, 
+        enum: ['Family', 'Caregiver'], 
+        required: true
+    },
+    created_by: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'MainUser', 
+        required: true
+    },
+    invitationToken: { 
+        type: String
+    },
+    isActive: { 
+        type: Boolean, 
+        default: false
+    }
+}, { 
+    timestamps: true
+});
 
 const ContactUser = mongoose.model('ContactUser', contactSchema)
 
--- backend-side/routes/mainUserRoutes.js
+++ backend-side/routes/mainUserRoutes.js
@@ -1,44 +1,10 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-
-import MainUser from '../models/MainUser.js';
-
+import { registerUser, loginUser} from '../controller/mainUserController.js';
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    try {
-        const { fullname, email, password } = req.body;
+// registration 
+router.post('/register', registerUser)
 
-        const existing = await MainUser.findOne({ email });
-        if (existing) return res.status(400).json({ message: 'Email already registered' })
-
-        const salt = await bcrypt.genSalt(10)
-        const hashed = await bcrypt.hash(password, salt)
-            
-        const newAdmin = new MainUser({ fullname, email, password: hashed, role: 'admin' })
-        await newAdmin.save();
-        res.status(201).json({ message: 'Admin registered' })
-    } catch (error) {
-        res.status(500).json({ message: 'Server error'})
-    }
-});
-
-router.post('/login', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-
-        const user = await MainUser.findOne({ email })
-        if (!user) return res.status(400).json({ message: 'Invalid or password' })
-
-        const valid = await bcrypt.compare(password, user.password)
-        if (!valid) return res.status(400).json({ message: 'Invalid email or password'})
-
-        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' })
-        res.json({ token, user: { id: user._id, email: user.email, fullname: user.fullname } })
-    } catch (error) {
-        res.status(500).json({message: 'Server error'})
-    }
-});
+router.post('/login', loginUser)
 
 export default router
