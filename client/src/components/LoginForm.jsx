c027f9ae6e0b264ad0dad66f427c2e04fe6a9eae
hannamalana98 <hannamalana98@gmail.com>
2025-05-20 11:46:55 +0800

    feat: design changes in log in and sign up, sign up navigation button to <a> tag

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -15,20 +15,12 @@ export default function LoginForm({onLogin, setIsLogin, navigate}) {
                 password,
             });
             console.log('Login successfully:', response.data)
-<<<<<<< HEAD
             const token = response.data.token || response.data.accessToken;
             if (token) { //keeps you logged in after refresh
               localStorage.setItem('userToken', token);
               onLogin();
               navigate('/home');
             }
-=======
-            if (response.data.token) {
-              localStorage.setItem('userToken', response.data.token);
-            }
-            onLogin() // check if app was logged in.
-            // navigate('/home')
->>>>>>> main
         } catch (error) {
             console.error('Login error', error.response?.data || error.message)
         }
@@ -41,9 +33,10 @@ export default function LoginForm({onLogin, setIsLogin, navigate}) {
 
     return(
         <>
-        <form onSubmit={handleSubmit} className='border-2 border-white rounded-xl p-8 w-150 h-110 bg-blue-800/80 backdrop-blur-md shadow-lg'>
-            <h2 className="text-xl font-bold mb-4 text-center text-white">Login Page</h2>
-            <h3 className="mb-4 text-white text-center">Elderly Care ❤️</h3>
+      
+        <form onSubmit={handleSubmit} className='flex flex-col justify-center border-2 border-white rounded-xl p-8 w-150 h-110 bg-blue-800/80 backdrop-blur-md shadow-lg'>
+            {/* <h2 className="text-xl font-bold mb-4 text-center text-white">Login Page</h2> */}
+            {/* <h3 className="mb-4 text-white text-center">Elderly Care ❤️</h3> */}
             <div className="mb-4">
               <label className="block text-white mb-1" htmlFor="loginEmail">Email</label>
               <input
@@ -68,13 +61,9 @@ export default function LoginForm({onLogin, setIsLogin, navigate}) {
             </div>
             <div className="mb-4 text-sm text-white text-center">
               Don't have an account?{' '}
-              <button
-              type="button"
-                className="text-blue-300 underline"
-                onClick={() => setIsLogin(false)}
-              >
-                signup
-              </button>
+              <a href="#" onClick={(e) => { e.preventDefault(); setIsLogin(false); }} className="text-blue-200 hover:underline cursor-pointer">
+                Signup
+                </a>
             </div>
             <button 
             onClick={handleLogin}
