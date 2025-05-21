e1977cfd508eab4562b4ebd17566c883d5aeaa78
hannamalana98 <hannamalana98@gmail.com>
2025-05-21 19:39:26 +0800

    medical records

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,8 +1,11 @@
-// Import Firebase modules you need
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
 
-// Your Firebase config (replace with your actual config)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCvierWRseV0ALtyyH7y0x6SsRvBtx6Rq8",
   authDomain: "medicalrecords-1e272.firebaseapp.com",
@@ -13,21 +16,7 @@ const firebaseConfig = {
   measurementId: "G-898TVC1YKV"
 };
 
-// Initialize Firebase app
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Storage service
-const storage = getStorage(app);
-
-// Export storage to use in other files
-export { storage };
-
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+export default app
